1dc0390877dc694ee08592e726748a93bf5b8296
Stephane Vanraes <stephane.vanraes@gmail.com>
2020-03-24 11:33:33 +0100

    use js based contact submission

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,6 +1,34 @@
 <script>
     import Button from '../components/Button.svelte'
     import TextField from '../components/TextField.svelte'
+
+    let name;
+    let email;
+    let subject;
+    let body;
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+    function handleSubmit() {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ 
+            "form-name": "contact",
+            "name": name,
+            "email": email,
+            "subject": subject,
+            "body": body
+        })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+    };
 </script>
 
 <style>
@@ -15,11 +43,11 @@
     } 
 </style>
 
-<form name="contact" action="/contacted" method="POST" data-netlify="true">
-    <TextField label="What can I call you ?" name="name" required />
-    <TextField label="Where can I reach you ?" name="email" required />
-    <TextField label="What is this about ?" name="subject" required />
-    <TextField label="Here you write your message" name="body" rows="5" type="textarea" required />
+<form>
+    <TextField label="What can I call you ?" name="name" required bind:value="{name}" />
+    <TextField label="Where can I reach you ?" name="email" required bind:value="{email}" />
+    <TextField label="What is this about ?" name="subject" required bind:value="{subject}" />
+    <TextField label="Here you write your message" name="body" rows="5" type="textarea" required bind:value="{body}" />
     <div>
         <Button type="submit">Send</Button>
     </div>
