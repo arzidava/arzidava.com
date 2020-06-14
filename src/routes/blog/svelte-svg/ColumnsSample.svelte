72eff1d9379a585f4c014c8bdeb87ba2605d0c5d
stephane-vanraes <stephane.vanraes@gmail.com>
2020-06-14 17:38:07 +0200

    Add SVG article

---
+++ src/routes/blog/svelte-svg/ColumnsSample.svelte
@@ -0,0 +1,101 @@
+<script>
+    import Columns from './Columns.svelte'
+    export let interactive
+
+    let data = [15, 25, 40, 78, 100]
+    let input = 0
+
+    const handleClick = () => {
+        data.push(input)
+        data = data
+        input = 0
+    }
+    const handleKey = (ev) => {
+        if (ev.key === 'Enter') {
+            handleClick()
+        }
+    }
+    const remove = (i) => {
+        data.splice(i, 1)
+        data = data
+    }
+
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    .wrapper > :global(*) {
+        margin: 1rem;
+    }
+    .wrapper > :global(svg) {
+        flex: 0 0 200px;
+        height: 200px;
+    }
+    .interaction {
+        display: flex;
+        flex: 1 0;
+        flex-direction: column;
+    }
+    button {
+        background: transparent;
+        border: none;
+        color: white;
+    }
+    input {
+        color: black;
+        padding: .25rem;
+        text-align: center;
+    }
+    ul {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(8ch, 1fr));
+        list-style-type: none;
+    }
+    li {
+        background-color: var(--white);
+        color: black;
+        display: flex;
+        flex-direction: row;
+        margin: .25rem;
+        padding: .25rem;
+    }
+    li > span {
+        flex: 1 0;
+    }
+    li > button {
+        color: red;
+    }
+    li > button:focus,
+    li > button:hover {
+        outline: none;
+    }
+</style>
+
+<div class="wrapper">
+    <Columns data="{data}" />
+    {#if interactive}
+    <div class="interaction">
+        <div>
+            <label>
+                <span>Add a value</span>
+                <input type="number" min="0" max="1000" bind:value="{input}" on:keyup="{handleKey}">
+            </label>
+
+            <button type="button" on:click="{handleClick}">Add</button>
+        </div>
+        <ul>
+            {#each data as value, i}
+                <li>
+                    <span>{value}</span>
+                    <button type="button" on:click="{() => remove(i)}">X</button>
+                </li>
+            {/each}
+        </ul>
+    </div>
+    {/if}
+</div>
+
