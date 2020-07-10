c5e156e30337110fb1932346146a27ba74303072
Stephane Vanraes <stephane.vanraes@fjordline.com>
2020-07-10 09:20:32 +0200

    add warning component

---
+++ src/components/Warning.svelte
@@ -0,0 +1,13 @@
+<style>
+    div {
+        background-color: var(--secondary);
+        border-left: 4px solid var(--secondary-light);
+        border-radius: 4px;
+        margin: 1rem 0;
+        padding: .75rem 1.5rem 1rem 1.5rem;
+    }
+</style>
+
+<div>
+    <slot></slot>
+</div>
