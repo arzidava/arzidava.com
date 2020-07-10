000af28012ffc0f039a6c661ef4bc9d31c6f4462
stephane-vanraes <stephane.vanraes@gmail.com>
2020-07-10 21:32:08 +0200

    start of redesign

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,5 @@
 <script>
-	import Nav from '../components/Navigation/Nav.svelte';
-	import '../utils/worklets/bubbles.js'
-	
-	export let segment;
 </script>
-
-<Nav {segment}/>
 
 <main>
 	<slot></slot>
--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -1,32 +1,106 @@
 <script>
-    import About from './About.svelte'
-    import Articles from './Articles.svelte'
+    import MultitoneImage from 'svelte-multitoneimage'
     import Header from '../../components/Header.svelte'
-
-    export let posts;
+    import Logo from '../../components/Logo.svelte'
+	const b = '#404040'
+	const w= '¤fcfcfc'
+	const p = '#005670'
+    const pl = '#0491BD'
+	const s = '#BD6004'
+	const sl = '#FF8F1F'
+	const a = '#F4442E'
+	const sp = '#805224'
 </script>
 
 <style>
-    div {
+    .wrapper {
         display: flex;
-        flex-direction: row;
+        height: 100vh;
+        position: fixed;
+        width: 100vw;
+    }
+    .wrapper > :global(img) {        
+        flex: 1 0 50%;
+        object-fit: cover;
+		width: 50%;
+		z-index: -1;
+    }
+    .wrapper > :global(img:first-of-type) { 
+        object-position: 100%;
+    }
+    .wrapper > :global(img:last-of-type) { 
+        object-position: 0%;
+    }
+    .content {
+        color: white;
+        display: flex;
         flex-wrap: wrap;
+        left: 50%;
+        margin-top: 3rem;
+        position: absolute;
+        text-align: center;
+        transform: translateX(-50%);	
+		width: 315px;
+    }
+    .content > :global(h1) {
+        font-weight: bold;
+        padding-right: 0rem !important;
+        text-align: center;
+		width: 100%;
+    }
+    nav {
+        display: contents;
+    }
+    a {
+        background-color: black;
+        display: inline-block;
+        flex: 1 0 calc(50% - 10px);
+        font-size: 1.25rem;
+		margin: 5px;
+        padding: .5rem 1rem;
     }
 
-    div > :global(*) {
-        flex: 1 0 calc(275px);
-        margin: 1rem .5rem 0;
+    a:hover {
+        box-shadow: 2px 2px black;
+    }
+
+    a:nth-child(2n+1) {
+        background-color: var(--secondary-light);
+    }
+    a:nth-child(2n+1):hover {
+        background-color: var(--secondary);
+    }
+    a:nth-child(2n) {
+        background-color: var(--primary-light);
+    }
+    a:nth-child(2n):hover {
+        background-color: var(--primary);
+    }
+
+    @media screen and (max-width: 430px) {
+        .wrapper > :global(img:last-of-type) {
+            display: none;
+        }
+        a {
+            flex: 1 0 100%;
+        }
+        a:nth-child(2n) {
+            background-color: var(--secondary-light);
+        }
     }
 </style>
 
-<svelte:head>
-    <title>arzidava</title>
-    <meta name="description" content="The personal website of Stephane Vanraes, with articles on frontend development subjects.">
-</svelte:head>
+<div class="wrapper">
+	<MultitoneImage src="./lyderhorn1.png" colours={[b,pl,w]} amplitude="1" exponent="1.1"/>
+	<MultitoneImage src="./lyderhorn2.png" colours={[b,sl,w]} amplitude="1" exponent="1.1"/>
+</div>
 
-<Header>arzidava (staging)</Header>
-
-<div>
-    <About />
-    <Articles {posts} />
+<div class="content">
+    <Header>arzidava</Header>
+    <nav>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
+        <a href="/concepts">concepts</a>
+        <a href="/blog">blog</a>
+    </nav>
 </div>
