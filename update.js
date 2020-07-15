5084fd48d2c7ac639a1d5684b2008c3962183377
Stephane Vanraes <stephane.vanraes@fjordline.com>
2020-07-15 12:40:02 +0200

    change from dist to public

--- update.js
+++ update.js
@@ -6,7 +6,7 @@ sh.exec('npx degit arzidava/concepts concepts');
 
 // copy concepts page
 sh.mkdir('__sapper__')
-sh.cp('-r', 'concepts/dist', '__sapper__/export')
+sh.cp('-r', 'concepts/public', '__sapper__/export')
 
 // cleanup
 sh.rm('-rf', 'concepts')
