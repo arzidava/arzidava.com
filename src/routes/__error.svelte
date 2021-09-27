30b3958042b82dd3e42472460be105d530c98cf2
stephane-vanraes <stephane.vanraes@gmail.com>
2021-09-27 18:13:38 +0200

    add error page, remove sapper article

---
+++ src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<div>
+	<h1>Nothing to see here but desolation and emptyness.</h1>
+</div>
+
+<style>
+	div {
+		background-image: url(/images/jotunheimen.png);
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		margin: -1rem;
+	}
+	h1 {
+		background: #fff7;
+		font-size: 4rem;
+		padding: 1rem;
+		text-align: center;
+		width: 100%;
+	}
+</style>
